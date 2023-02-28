8a07154899d7af6eec0a73ea2aecef1a525deec3
Arhizmei <arhizmei@gmail.com>
2023-02-28 12:35:31 +0200

    good

--- app/src/main/java/com/zmei/practice1/MainActivity.kt
+++ app/src/main/java/com/zmei/practice1/MainActivity.kt
@@ -2,10 +2,62 @@ package com.zmei.practice1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.zmei.practice1.databinding.ActivityMainBinding
+import constans.constans
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    binding.button1.setOnClickListener {
+
+
+    var userName = binding.editText1.text.toString()
+
+    when(userName){
+
+    constans.director -> {
+        binding.textView.visibility = View.VISIBLE
+
+        if (constans.directorParol == binding.editText2.text.toString()) {
+            binding.imageView.visibility = View.VISIBLE
+            binding.imageView.setImageResource(R.drawable.vadim)
+            val currency = constans.directorCurency
+            binding.textView.text = "Ваша зарплата ${currency} $"
+        } else {
+            binding.imageView.visibility = View.VISIBLE
+            binding.textView.text = "неверный пароль"
+            binding.imageView.setImageResource(R.drawable.stop)
+        }
     }
-}
+    constans.dvornik -> {
+            binding.textView.visibility = View.VISIBLE
+
+            if (constans.dvornikParol == binding.editText2.text.toString()) {
+                binding.imageView.visibility = View.VISIBLE
+                binding.imageView.setImageResource(R.drawable.andrey)
+                val currency = constans.dvornikCurency
+                binding.textView.text = "Ваша зарплата ${currency} $"}
+            else { binding.textView.text = "неверный пароль"
+                binding.imageView.visibility = View.VISIBLE
+                binding.imageView.setImageResource(R.drawable.stop)}
+        }
+    else -> {
+        binding.textView.visibility = View.VISIBLE
+        binding.imageView.setImageResource(R.drawable.stop)
+        binding.textView.text = "Такого работника нет"
+    }
+    }
+    }
+
+    }
+
+
+    }
+
+
---
+++ app/src/main/java/constans/constans.kt
@@ -0,0 +1,11 @@
+package constans
+
+object constans {
+
+    const val director = "Vadim"
+    const val dvornik = "Andrey"
+    const val directorCurency = "5000"
+    const val dvornikCurency = "500"
+    const val directorParol = "333666"
+    const val dvornikParol = "111111"
+}
